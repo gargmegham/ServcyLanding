ce0ab00a184e18001ce4ff768e0e1f3815bfc520
gargmegham <gargmegham3@gmail.com>
2023-01-01 18:05:03 +0530

    updated header

---
+++ components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <img src="@/static/logo.svg" width="80px" class="logo" />
+        <v-spacer></v-spacer>
+        <button>Sign In</button>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+header {
+    height: 18vh;
+    padding: 10px 5%;
+    background: rgb(167,226,165);
+    background: -moz-linear-gradient(90deg, rgba(167,226,165,1) 0%, rgba(91,169,89,1) 23%, rgba(77,126,62,1) 58%, rgba(38,84,47,1) 100%);
+    background: -webkit-linear-gradient(90deg, rgba(167,226,165,1) 0%, rgba(91,169,89,1) 23%, rgba(77,126,62,1) 58%, rgba(38,84,47,1) 100%);
+    background: linear-gradient(90deg, rgba(167,226,165,1) 0%, rgba(91,169,89,1) 23%, rgba(77,126,62,1) 58%, rgba(38,84,47,1) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#a7e2a5",endColorstr="#26542f",GradientType=1);
+}
+.logo {
+    vertical-align: middle;
+    height: 100%;
+}
+</style>
