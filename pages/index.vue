b3df8a2ada747f6b7a1c07a3ebfb5c70501a6d25
gargmegham <gargmegham3@gmail.com>
2023-01-13 12:45:23 +0530

    add email field cta

--- pages/index.vue
+++ pages/index.vue
@@ -101,6 +101,17 @@
             height="100%"
           />
         </v-row>
+        <form class="newsletter-form mt-8">
+          <input
+            id="cta-input"
+            v-model="email"
+            type="email"
+            placeholder="Your Email address"
+          />
+          <button id="cta-button" @click="submit" :disabled="!isEmailValid">
+            <span id="cta-span">I'm Interested</span>
+          </button>
+        </form>
       </v-col>
       <v-col sm="12" cols="12" lg="6" xl="6" md="6"></v-col>
     </v-row>
@@ -110,10 +121,27 @@
 <script>
 export default {
   name: "HomePage",
+  data() {
+    return {
+      email: "",
+    };
+  },
+  computed: {
+    isEmailValid() {
+      var validRegex =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return !!this.email.match(validRegex);
+    },
+  },
+  methods: {
+    submit(e) {
+      e.preventDefault();
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style scoped lang="css">
 .servcy-body {
   min-height: 90vh;
   padding-left: 5%;
@@ -154,6 +182,75 @@ li {
   }
   .list-style-none {
     list-style-type: none;
+  }
+}
+</style>
+
+<style scoped lang="scss">
+.newsletter-form {
+  --primary: #275efe;
+  --input-placeholder: #7e84a3;
+  --input-text: #7e84a3;
+  --border-default: #e1e6f9;
+  --background: #fff;
+  --button-text: #ffffff;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  background: var(--background);
+  box-shadow: inset 0 0 0 var(--border-width, 1px)
+    var(--border, var(--border-default));
+  border-radius: 9px;
+  padding-right: 4px;
+  transition: box-shadow 0.25s;
+  &:focus-within {
+    --border-width: 1.5px;
+  }
+  #cta-input,
+  #cta-button {
+    background: none;
+    outline: none;
+    display: block;
+    border: none;
+    font-family: inherit;
+    font-size: 14px;
+    line-height: 24px;
+    margin: 0;
+  }
+  #cta-input {
+    width: 100%;
+    flex-grow: 1;
+    padding: 12px 12px 12px 16px;
+    color: var(--input-text);
+    font-weight: 400;
+    &::placeholder {
+      color: var(--input-placeholder);
+    }
+  }
+  #cta-button {
+    --text-opacity: 1;
+    --border-radius: 7px;
+    position: relative;
+    padding: 8px 0;
+    min-width: 130px;
+    text-align: center;
+    font-weight: 600 !important;
+    opacity: var(--button-opacity, 0.5);
+    filter: var(--button-filter, grayscale(65%));
+    color: var(--button-text);
+    border-radius: var(--border-radius);
+    transform: translateZ(0);
+    transition: opacity 0.25s, filter 0.25s;
+    -webkit-tap-highlight-color: transparent;
+    &:not(.active) {
+      background: var(--primary);
+    }
+    #cta-span {
+      display: block;
+      position: relative;
+      z-index: 4;
+      opacity: var(--text-opacity);
+    }
   }
 }
 </style>
