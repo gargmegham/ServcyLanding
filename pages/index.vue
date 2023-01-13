ae5946aa05e1261ecc78a982e89f10423aa32e34
gargmegham <gargmegham3@gmail.com>
2023-01-13 19:20:42 +0530

    responsive integration icons

--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@
             width="90"
             height="100%"
           />
-          <v-tooltip bottom>
+          <v-tooltip bottom v-if="!$vuetify.breakpoint.smAndDown">
             <template v-slot:activator="{ on }">
               <img
                 src="@/assets/icons/figma-logo.svg"
