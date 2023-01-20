f93d0c779114db9e5965710ba22540c8dba4fc26
gargmegham <gargmegham3@gmail.com>
2023-01-20 19:44:15 +0530

    features page

--- components/FeaturesSection.vue
+++ components/FeaturesSection.vue
@@ -1,86 +1,34 @@
 <template>
   <section id="features" class="s-vh-100">
-    <div class="d-flex flex-wrap align-center justify-center">
-      <v-sheet
-        color="white"
-        elevation="2"
-        :height="$vuetify.breakpoint.smAndDown ? '' : '450px'"
-        width="350px"
-        rounded="xl"
-        class="pa-6 card-bounce ma-4 text-center"
-      >
-        <img
-          src="@/assets/icons/calendly-logo.svg"
-          alt="gear"
-          class="mx-auto d-block align-items-center"
-          width="100px"
-        />
-        <div class="dark--text s-title s-bold mt-3">
-          Schedule meetings with less back and forth
-        </div>
-        <div class="dark--text mt-6 s-body">
-          Simply set your availability and share your link so clients can find a
-          time that works. Your clients will thank (and book) you.
-        </div>
-        <div class="dark--text mt-4 s-body">
-          Meeting with your client increases your chances of winning their
-          business by 40%
-        </div>
-      </v-sheet>
-      <v-sheet
-        color="white"
-        elevation="2"
-        height="500px"
-        width="350px"
-        rounded="xl"
-        class="pa-6 ma-4 card-bounce text-center"
-      >
-        <img
-          src="@/assets/icons/gear.svg"
-          alt="gear"
-          class="mx-auto d-block align-items-center"
-          width="100px"
-        />
-        <div class="dark--text s-title s-bold mt-3">
-          All-In-One Place. Consolidate Your Software Stack
-        </div>
-        <div class="dark--text mt-6 s-body">
-          One login. One platform. One software. No more splitting your efforts
-          throughout multiple apps just because your clients are on different
-          platforms.
-        </div>
-        <div class="dark--text mt-4 s-body">
-          Manage finances, tasks, projects, and take your business to next
-          level.
-        </div>
-      </v-sheet>
-      <v-sheet
-        color="white"
-        elevation="2"
-        :height="$vuetify.breakpoint.smAndDown ? '' : '450px'"
-        width="350px"
-        rounded="xl"
-        class="pa-6 ma-4 card-bounce text-center"
-      >
-        <img
-          src="@/assets/icons/app-icon.svg"
-          alt="gear"
-          class="mx-auto d-block align-items-center"
-          width="100px"
-        />
-        <div class="dark--text s-title s-bold mt-3">
-          Keep Your Clients Always Up-To-Date
-        </div>
-        <div class="dark--text mt-6 s-body">
-          Eliminate another client concern 'what are you working on?' with the
-          Client Dashboard: an instant and intuitive way for your client to
-          check on work progress, tasks, projects, results and more.
-        </div>
-        <div class="dark--text mt-4 s-body">
-          Reduce unnecessary communication and increase your client's
-          satisfaction. No more manual updates or pestering emails.
-        </div>
-      </v-sheet>
+    <div class="wave-block text-center">
+      <div class="servcy-sub-headline-font s-bold font-castoro pt-10 pb-6">
+        Features
+      </div>
+      <div class="padding-x d-flex flex-row justify-center flex-wrap">
+        <v-sheet
+          v-for="(value, title, index) in features"
+          :key="index"
+          width="300px"
+          height="300px"
+          class="card pa-6 ma-4"
+        >
+          <h3 class="card-title s-bold font-castoro">{{ title }}</h3>
+          <div class="mx-auto my-3">
+            <v-btn fab outlined color="silver">
+              <v-icon>{{ value.icon }}</v-icon></v-btn
+            >
+          </div>
+          <ul class="mt-4">
+            <li
+              v-for="feature in value.val"
+              class="s-body text-left"
+              :key="feature"
+            >
+              {{ feature }}
+            </li>
+          </ul>
+        </v-sheet>
+      </div>
     </div>
   </section>
 </template>
@@ -88,19 +36,126 @@
 <script>
 export default {
   name: "FeaturesSection",
+  data() {
+    return {
+      features: {
+        "Schedule A Meeting": {
+          icon: "mdi-calendar",
+          val: [
+            "Set your availability.",
+            "Share your meeting link.",
+            "Get scheduled.",
+          ],
+        },
+        "Sign Your Contract": {
+          icon: "mdi-file-sign",
+          val: [
+            "Share, sign & store on the platform.",
+            "Use provided templates or import your own.",
+            "Tweak style and format to suit your business.",
+          ],
+        },
+        "Create Tasks": {
+          icon: "mdi-checkbox-marked-circle-plus-outline",
+          val: [
+            "Create new tasks and subtasks in seconds.",
+            "Attach links, PRs, files and more.",
+            "Keep everyone informed about the current state of your projects.",
+          ],
+        },
+        "Track Time": {
+          icon: "mdi-timer",
+          val: [
+            "Track time spent on tasks.",
+            "This data can be used to generate invoices.",
+            "You can also use it to track your efficiency.",
+          ],
+        },
+        "Raise Invoices": {
+          icon: "mdi-receipt-text-plus",
+          val: [
+            "Use provided templates or import your own.",
+            "Tweak style and format to suit your business.",
+            "Add automatic reminders.",
+          ],
+        },
+        "Get Paid": {
+          icon: "mdi-cash-multiple",
+          val: [
+            "Bill your clients directly from inside the platform.",
+            "Automated recurring billing for retainers.",
+            "Integrates with Stripe, PayPal and bank accounts.",
+          ],
+        },
+        "Track Earnings For Taxes": {
+          icon: "mdi-currency-usd",
+          val: [
+            "Set  goals for revenue and track progress.",
+            "Revenue reports for taxation purposes.",
+            "Insights based on tracked time.",
+          ],
+        },
+        "Inbox Aggregation": {
+          icon: "mdi-inbox-arrow-down",
+          val: [
+            "Comments from figma, github, linear, asana, notion & more..",
+            "Emails from mutiple accounts aggregated & assigned to one client.",
+            "Payments information aggregated in one place.",
+          ],
+        },
+      },
+    };
+  },
 };
 </script>
 
 <style scoped>
 #features {
-  padding: 50px 5%;
+  padding-bottom: 50px;
+  font-family: "Open Sans", sans-serif;
 }
-.card-bounce {
-  transition: 0.6s cubic-bezier(0.47, 2.02, 0.31, -0.36);
+.card-title,
+li {
+  color: #60636d;
 }
-.card-bounce:hover,
-.card-bounce:focus,
-.card-bounce:active {
-  transform: scale(1.1);
+.padding-x {
+  padding-left: 5%;
+  padding-right: 5%;
+}
+.wave-block {
+  margin-bottom: 50px;
+  background-image: url("@/assets/icons/wave.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.card {
+  background: rgb(255, 255, 255);
+  background: -moz-linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(223, 224, 236, 1) 47%
+  );
+  background: -webkit-linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgb(236, 223, 234) 47%
+  );
+  background: linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(223, 224, 236, 1) 47%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#ffffff",endColorstr="#dfe0ec",GradientType=1);
+  border-radius: 10px;
+  box-shadow: -1rem 0 3rem #7e84a3;
+  transition: 0.4s ease-out;
+}
+.card:hover {
+  transform: translateY(-20px);
+  transition: 0.4s ease-out;
+}
+
+.card:hover ~ .card {
+  transition: 0.4s ease-out;
 }
 </style>
--- components/WhyServcy.vue
+++ components/WhyServcy.vue
@@ -18,6 +18,88 @@
         />
       </div>
     </div>
+    <div class="d-flex flex-wrap align-center justify-center">
+      <v-sheet
+        color="white"
+        elevation="2"
+        :height="$vuetify.breakpoint.smAndDown ? '' : '430px'"
+        width="350px"
+        rounded="xl"
+        class="pa-6 card-bounce ma-4 text-center"
+      >
+        <img
+          src="@/assets/icons/calendly-logo.svg"
+          alt="gear"
+          class="mx-auto d-block align-items-center"
+          width="100px"
+        />
+        <div class="dark--text s-title s-bold mt-3">
+          Schedule meetings with less back and forth
+        </div>
+        <div class="dark--text mt-6 s-body">
+          Simply set your availability and share your link so clients can find a
+          time that works. Your clients will thank (and book) you.
+        </div>
+        <div class="dark--text mt-4 s-body">
+          Meeting with your client increases your chances of winning their
+          business by 40%
+        </div>
+      </v-sheet>
+      <v-sheet
+        color="white"
+        elevation="2"
+        :height="$vuetify.breakpoint.smAndDown ? '' : '450px'"
+        width="350px"
+        rounded="xl"
+        class="pa-6 ma-4 card-bounce text-center"
+      >
+        <img
+          src="@/assets/icons/gear.svg"
+          alt="gear"
+          class="mx-auto d-block align-items-center"
+          width="100px"
+        />
+        <div class="dark--text s-title s-bold mt-3">
+          All-In-One Place. Consolidate Your Software Stack
+        </div>
+        <div class="dark--text mt-6 s-body">
+          One login. One platform. One software. No more splitting your efforts
+          throughout multiple apps just because your clients are on different
+          platforms.
+        </div>
+        <div class="dark--text mt-4 s-body">
+          Manage finances, tasks, projects, and take your business to next
+          level.
+        </div>
+      </v-sheet>
+      <v-sheet
+        color="white"
+        elevation="2"
+        :height="$vuetify.breakpoint.smAndDown ? '' : '430px'"
+        width="350px"
+        rounded="xl"
+        class="pa-6 ma-4 card-bounce text-center"
+      >
+        <img
+          src="@/assets/icons/app-icon.svg"
+          alt="gear"
+          class="mx-auto d-block align-items-center"
+          width="100px"
+        />
+        <div class="dark--text s-title s-bold mt-3">
+          Keep Your Clients Always Up-To-Date
+        </div>
+        <div class="dark--text mt-6 s-body">
+          Eliminate another client concern 'what are you working on?' with the
+          Client Dashboard: an instant and intuitive way for your client to
+          check on work progress, tasks, projects, results and more.
+        </div>
+        <div class="dark--text mt-4 s-body">
+          Reduce unnecessary communication and increase your client's
+          satisfaction. No more manual updates or pestering emails.
+        </div>
+      </v-sheet>
+    </div>
   </section>
 </template>
 
@@ -28,6 +110,9 @@ export default {
 </script>
 
 <style scoped>
+#why-servcy {
+  margin-bottom: 50px;
+}
 .why-servcy-desc {
   padding-left: 10%;
   padding-right: 10%;
@@ -42,6 +127,14 @@ export default {
   background-image: url("@/assets/icons/wave.svg");
   background-repeat: no-repeat;
   background-size: cover;
+}
+.card-bounce {
+  transition: 0.6s cubic-bezier(0.47, 2.02, 0.31, -0.36);
+}
+.card-bounce:hover,
+.card-bounce:focus,
+.card-bounce:active {
+  transform: scale(1.1);
 }
 @media screen and (max-width: 768px) {
   .wave-block {
