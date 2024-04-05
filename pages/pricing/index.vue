916d22652d44bd2854c5b033dcd9a91feefccc96
Megham Garg <gargmegham3@gmail.com>
2024-04-05 16:27:34 +0530

    Add pricing page with styles and image in index.vue

---
+++ pages/pricing/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="servcy-container">
+    <section class="pt-[20vh] pb-[10vh]">
+      <div class="flex flex-col items-center text-center">
+        <div class="font-axiforma font-extrabold md:text-5xl text-4xl">
+          Pricing<br />
+          <span class="font-axiforma font-extrabold md:text-3xl text-2xl"
+            >that works for you—today and tomorrow</span
+          >
+        </div>
+      </div>
+      <div class="mt-16">
+        <img
+          src="/shots/kanban.svg"
+          alt="hero-image"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pricing",
+};
+</script>
+
+<style scoped>
+.servcy-container > * {
+  padding-left: 5%;
+  padding-right: 5%;
+  background-color: #032123;
+}
+</style>
