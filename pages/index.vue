309b8985eacea02bbebe24ca91eba54a6ec963a2
Megham Garg <gargmegham3@gmail.com>
2024-04-05 12:39:46 +0530

    Update components: CentralPaymentHub.vue, CentralTimeHub.vue, MergedCalendarView.vue, CentralDocumentHub.vue, CentralTaskHub.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="servcy-container">
     <Hero />
+    <Dashboard />
     <UnifiedInbox />
-    <CentralTaskHub />
-    <CentralDocumentHub />
-    <CentralPaymentHub />
-    <CentralTimeHub />
-    <MergedCalendarView />
     <FinalCTA />
   </div>
 </template>
@@ -14,11 +10,7 @@
 <script>
 import Hero from "@/components/Sections/Hero.vue";
 import UnifiedInbox from "@/components/Sections/UnifiedInbox.vue";
-import CentralTaskHub from "@/components/Sections/CentralTaskHub.vue";
-import CentralDocumentHub from "@/components/Sections/CentralDocumentHub.vue";
-import CentralPaymentHub from "@/components/Sections/CentralPaymentHub.vue";
-import CentralTimeHub from "@/components/Sections/CentralTimeHub.vue";
-import MergedCalendarView from "@/components/Sections/MergedCalendarView.vue";
+import Dashboard from "@/components/Sections/Dashboard.vue";
 import FinalCTA from "@/components/Sections/FinalCTA.vue";
 
 export default {
@@ -26,26 +18,14 @@ export default {
   components: {
     Hero,
     FinalCTA,
+    Dashboard,
     UnifiedInbox,
-    CentralPaymentHub,
-    CentralTimeHub,
-    MergedCalendarView,
-    CentralTaskHub,
-    CentralDocumentHub,
   },
 };
 </script>
 
 <style scoped>
-.servcy-container {
-  scroll-snap-type: y mandatory;
-  overflow-y: scroll;
-  height: 100vh;
-}
-
 .servcy-container > * {
-  scroll-snap-align: start;
-  height: 100vh;
   padding-left: 5%;
   padding-right: 5%;
   background-color: #032123;
