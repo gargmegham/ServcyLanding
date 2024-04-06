b8cc300ae6f3fb7e2c41c5469f17c47f5742f156
Megham Garg <gargmegham3@gmail.com>
2024-04-06 17:18:52 +0530

    Update styles and indentation in [...slug].vue and [slug].vue

--- pages/documents/[...slug].vue
+++ pages/documents/[...slug].vue
@@ -3,46 +3,42 @@ const { slug } = useRoute().params
 </script>
 
 <template>
-    <article
-        class="servcy-docs flex justify-center bg-servcy-green px-[10%] pb-10 pt-[10vh] text-servcy-cream">
+    <article class="servcy-docs min-h-screen flex justify-center bg-servcy-green px-[10%] pb-10 pt-[10vh] text-servcy-cream">
         <ContentDoc :path="`/documents/${slug}`" v-slot="{ doc }">
             <div class="content mt-4 p-5">
-                <ContentRenderer :value="doc" />
+                <ContentRendererMarkdown :value="doc" />
             </div>
         </ContentDoc>
     </article>
 </template>
 
 <style>
-
-.servcy-docs .content h1 {
-    @apply text-3xl font-bold;
-    @apply text-servcy-wheat;
-}
-
-.servcy-docs .content h2 {
-    @apply text-2xl font-bold;
-}
-
-.servcy-docs .content h3 {
-    @apply text-xl font-bold;
-}
-
-.servcy-docs .content h4 {
-    @apply text-lg font-bold;
-}
-
-.servcy-docs .content h5 {
-    @apply text-base font-bold;
-}
-
-.servcy-docs .content a {
-    text-decoration: underline;
-    color: #d1bfae !important;
-}
-
-
 .servcy-docs .content {
+    h1 {
+        @apply text-3xl font-bold;
+        @apply text-servcy-wheat;
+    }
+
+    h2 {
+        @apply text-2xl font-bold;
+    }
+
+    h3 {
+        @apply text-xl font-bold;
+    }
+
+    h4 {
+        @apply text-lg font-bold;
+    }
+
+    h5 {
+        @apply text-base font-bold;
+    }
+
+    a {
+        text-decoration: underline;
+        color: #d1bfae !important;
+    }
 
     h1,
     h2,
@@ -53,16 +49,22 @@ const { slug } = useRoute().params
     p,
     ol,
     ul,
+    span,
+    div,
     pre,
     blockquote,
     table {
         margin-bottom: 20px;
     }
 
-    br {
-        margin-bottom: 12px;
+    ol,
+    ul {
+        li {
+            margin-top: 12px;
+            margin-bottom: 12px;
+        }
     }
 
-
     max-width: 850px !important;
-}</style>
+}
+</style>
