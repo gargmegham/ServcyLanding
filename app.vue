3f9109ddaf1ce3861eaec522808d31737ea754b9
Megham Garg <gargmegham3@gmail.com>
2024-04-11 08:31:38 +0530

    Update analytics scripts and add async attribute to script tag in app.vue

--- app.vue
+++ app.vue
@@ -8,14 +8,34 @@
 useHead({
     script: [
         {
-            type: "text/javascript",
-            async: true,
+            src: "https://www.googletagmanager.com/gtag/js?id=G-ZW4GBHWSKE",
+            async: true
+        },
+        {
             innerHTML: `
-                (function(c,l,a,r,i,t,y){
-                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "ffilkcgrum");
+            window.dataLayer = window.dataLayer || []
+            function gtag() {
+                dataLayer.push(arguments)
+            }
+            gtag("js", new Date())
+            gtag("config", "G-ZW4GBHWSKE")
+        `
+        },
+        {
+            type: "text/javascript",
+            innerHTML: `
+            (function (c, l, a, r, i, t, y) {
+                c[a] =
+                    c[a] ||
+                    function () {
+                        ;(c[a].q = c[a].q || []).push(arguments)
+                    }
+                t = l.createElement(r)
+                t.async = 1
+                t.src = "https://www.clarity.ms/tag/" + i
+                y = l.getElementsByTagName(r)[0]
+                y.parentNode.insertBefore(t, y)
+            })(window, document, "clarity", "script", "ffilkcgrum")
             `
         }
     ]
