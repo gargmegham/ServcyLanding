4b7ee6d362032f582de7e6ddb8fad00c07c79322
Megham Garg <gargmegham3@gmail.com>
2024-05-22 12:32:02 +0530

    refactor: Add "We're Open Source" link to landing page

--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -97,6 +97,14 @@
                 </div>
             </div>
         </ContentList>
+        <a
+            href="https://github.com/orgs/Servcy/repositories"
+            class="fixed bottom-4 right-4 !m-0 rounded-xl border border-neutral-200 !bg-black !px-2 !py-1 text-sm font-medium text-black dark:border-white/[0.2] dark:text-white">
+            <span>We're Open Source</span>
+            <Icon name="mdi:github" class="ml-2 h-6 w-6" />
+            <span
+                class="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-amber-500 to-transparent" />
+        </a>
     </div>
 </template>
 
